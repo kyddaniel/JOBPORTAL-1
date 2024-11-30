650902e1fd531eed044c6b594ea49864e01404af
kyddaniel <mksuenaa@connect.ust.hk>
2024-11-30 23:16:30 +0800

    Implement more componenets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import AllPosts from './components/AllPosts';
+import Create from './components/Create';
+import Edit from './components/Edit';
 import Search from './components/Search';
 
 function App() {
   return (
-    <div>
-      <Search/>
-    </div>
-  );
+    <>
+     <Navbar/>
+     <BrowserRouter>
+     <Routes>
+     <Route path='/' element={<AllPosts/>}/>
+     <Route path="/create" element={<Create />} />
+     <Route path="/edit" element={<Edit />} />
+     </Routes>
+     </BrowserRouter> 
+    </>
+    );
 }
 
 export default App;
